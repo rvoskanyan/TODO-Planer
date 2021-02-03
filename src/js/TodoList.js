295d2c86f6ea246b572884be0e1997c0cab7bb2c
rvoskanyan <rvoskanyan@mmtr.ru>
2021-02-03 11:14:33 +0300

    03.02.2021 - release addTask functional

--- src/js/TodoList.js
+++ src/js/TodoList.js
@@ -9,11 +9,14 @@ export default class TodoList {
     constructor(state) {
         this.date = state?.date;
         this.tasks = state?.tasks;
+        this.nextId = state?.nextId;
         this.createTasks();
         this.renderList();
     }
 
     createTasks() {
+        this.examplesTasks = [];
+
         if(this.tasks.length) {
             for(let elem of this.tasks) {
                 this.examplesTasks.push(new Task(
@@ -27,12 +30,22 @@ export default class TodoList {
     }
 
     renderList() {
+        this.nodesTasks = [];
+
         if(this.examplesTasks.length) {
+
+            const newContainer = document.createElement('div');
+            newContainer.className = 'list__content';
+            newContainer.id = nodeElements.container;
+
             for(let elem of this.examplesTasks) {
                 const node = elem.getNode();
+
+                newContainer.append(node);
                 this.nodesTasks.push(node);
-                document.getElementById(nodeElements.container).append(node);
             }
+
+            document.getElementById(nodeElements.container).replaceWith(newContainer);
         }
         else {
             console.log('Задачи не найдены!')
@@ -40,10 +53,12 @@ export default class TodoList {
     }
 
     toggleStatus(id) {
-        document.getElementById(`${nodeElements.prefixIdTask}${id}`).classList.toggle("item-content__input_delete");
         const index = this.tasks.findIndex(item => {
             return item.id === id;
         });
+
+        document.getElementById(`${nodeElements.prefixIdTask}${id}`).classList.toggle("item-content__input_delete");
+
         this.tasks[index].status = this.tasks[index].status === 0 ? 1 : 0;
         localStorage.setItem(lsKeyApp, JSON.stringify({
             date: this.date,
@@ -89,11 +104,44 @@ export default class TodoList {
         return this.tasks[index];
     }
 
-    addTask(task) {
-        this.tasks.push(task);
-        localStorage.setItem(lsKeyApp, JSON.stringify({
-            date: this.date,
-            tasks: this.tasks
-        }))
+    addTask() {
+        const div = document.createElement('div');
+        div.className = 'item-content';
+
+        const input = document.createElement('input');
+        input.type = 'text';
+        input.classList.add("item-content__input", "input");
+        input.addEventListener('blur', () => this.handleBlurNewTask(div, input));
+
+        const divControls = document.createElement('div');
+        divControls.classList.add("content__control", "inner-control");
+
+        div.prepend(input);
+        div.append(divControls);
+
+        document.getElementById(nodeElements.container).append(div);
+
+        input.focus();
+    }
+
+    handleBlurNewTask(node, field) {
+        if(field.value.length) {
+            this.tasks.push({
+                id: this.nextId,
+                text: field.value,
+                status: 0
+            })
+            this.nextId += 1;
+
+            localStorage.setItem(lsKeyApp, JSON.stringify({
+                date: this.date,
+                tasks: this.tasks,
+                nextId: this.nextId
+            }))
+
+            this.createTasks();
+            this.renderList();
+        }
+        node.remove();
     }
 }
--- src/js/app.js
+++ src/js/app.js
@@ -2,19 +2,16 @@ import '../styles/main.scss'
 import TodoList from './TodoList'
 import {lsKeyApp} from "./constants";
 
-/*if(/!*!localStorage.getItem(lsKeyApp)*!/true) {
+if(!localStorage.getItem(lsKeyApp)) {
     localStorage.setItem(lsKeyApp, JSON.stringify({
         date: new Date(),
-        tasks: [
-            {id: 1, text: 'test1', status: 0},
-            {id: 2, text: 'test2', status: 0},
-            {id: 3, text: 'test3', status: 0},
-            {id: 4, text: 'test4', status: 1},
-            {id: 5, text: 'test5', status: 0}
-        ]
+        tasks: [],
+        nextId: 1
     }))
-}*/
+}
 
 const state = JSON.parse(localStorage.getItem(lsKeyApp));
 
 const todoList = new TodoList(state);
+
+document.getElementById('addTask').addEventListener('click', todoList.addTask.bind(todoList));
