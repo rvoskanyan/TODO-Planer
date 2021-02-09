1d9c230d537b5449993a524450804508fbc8f791
rvoskanyan <rvoskanyan@mmtr.ru>
2021-02-09 18:25:40 +0300

    09.02.2021 - FEATURE-2 - connection AlaSQL

--- server/index.js
+++ server/index.js
@@ -1,5 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const alasql = require('alasql');
+
+alasql('CREATE TABLE tasks (id INT, text STRING, done BOOLEAN)');
+alasql("INSERT INTO tasks VALUES (1, 'Task Test!', true)");
+alasql("INSERT INTO tasks VALUES (2, 'Go to work!', false)");
+alasql("INSERT INTO tasks VALUES (3, 'Lunch!', true)");
+
+const results = alasql('SELECT * FROM tasks WHERE id > 1');
+
+console.log(results);
 
 const routes = require('./routes/routes');
 
